36fbc3043b5a7cbb52cef5508d38be18ce730ddd
Clément Cardonnel <clement.cardonnel@me.com>
2023-05-21 09:01:32 +0200

    Initial Commit

---
+++ AltitudeDemo/AltitudeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AltitudeDemoApp.swift
+//  AltitudeDemo
+//
+//  Created by Clément Cardonnel on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AltitudeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
